78aec355dc32d2c0615b099db5b7a2d7840fec5e
Suraj Rathi <suraj.rathi00@gmail.com>
2023-03-16 21:07:53 +0530

    clean up code and separate routines

--- iiwa_run/script/interior_motion.py
+++ iiwa_run/script/interior_motion.py
@@ -57,6 +57,12 @@ class Orchestrator:
                                                           -1.4368011875273539, -1.181244222973306, 2.0290526312957797,
                                                           -0.7484887519715488])
 
+        self.inserted_joint_state = JointState(header=Header(stamp=rospy.Time.now()),
+                                               name=active_joints,
+                                               position=[0.7974687140058682, -0.7059608762250215, -2.0557765983620215,
+                                                         -1.7324525588466884, -0.685107459772449, 1.136261558580683,
+                                                         -1.0476528028679626])
+
         self.move_group.set_end_effector_link("tool_link_ee")
 
     def enact_constraint(self):
@@ -70,59 +76,52 @@ class Orchestrator:
         self.scene.clear()
         self.scene.add_mesh("abdomen", abdomen_pose_transformed, str(p), size=(0.001, 0.001, 0.001))
 
+    def set_robot_state(self, state):
+        # Note: using wall clock here to allow the joint_state to be published and received
+        joint_pub = rospy.Publisher('/move_group/fake_controller_joint_states', JointState, queue_size=5)
+        for i in range(5):
+            joint_pub.publish(state)
+            rospy.rostime.wallsleep(0.1)
+        joint_pub.unregister()
+
     def transform_pose(self, msg: geometry_msgs.msg.PoseStamped) -> Pose:
         transform = self.tf_buffer.lookup_transform(self.move_group.get_planning_frame(),
                                                     msg.header.frame_id,
                                                     rospy.Time(0), rospy.Duration(0))
         return tf2_geometry_msgs.do_transform_pose(msg, transform).pose
 
-    def run(self):
-        # Note: using wall clock here to allow the joint_state to be published and received
-        print(self.move_group.get_current_joint_values())
+    # noinspection DuplicatedCode
+    @staticmethod
+    def error_code_to_string(code: Union[moveit_msgs.msg.MoveItErrorCodes, int]):
+        if isinstance(code, moveit_msgs.msg.MoveItErrorCodes):
+            code = code.val
+        codes = moveit_msgs.msg.MoveItErrorCodes
+        for name, val in codes.__dict__.items():
+            if val == code:
+                return name
+        rospy.logwarn(f"Invalid moveit error code: {code}")
+        return 'InvalidMOVEITError'
 
-        joint_pub = rospy.Publisher('/move_group/fake_controller_joint_states', JointState, queue_size=5)
-        for i in range(5):
-            joint_pub.publish(self.zero_joint_state)
-            rospy.rostime.wallsleep(0.1)
+    def execute(self, plan) -> Optional[float]:
+        t_start = time.time()
+        success = self.move_group.execute(plan, wait=True)
+        return time.time() - t_start if success else None
 
-        print(self.move_group.get_current_joint_values())
-
-        insertion_pose = geometry_msgs.msg.PoseStamped()
-        insertion_pose.header.frame_id = "Insertion_Pose"
-        insertion_pose.pose.orientation.w = 1
-
-        try:
-            insertion_pose.pose.position.z = -INITIAL_HEIGHT
-            self.move_group.set_start_state(RobotState(joint_state=self.zero_joint_state))
-            self.plan_and_execute(target=self.transform_pose(insertion_pose),
-                                  msg=f" for z={insertion_pose.pose.position.z:.2f}")
-
-            insertion_pose.pose.position.z = -SECOND_HEIGHT
-            self.move_group.set_start_state_to_current_state()
-            self.plan_and_execute(target=self.transform_pose(insertion_pose),
-                                  msg=f" for z={insertion_pose.pose.position.z:.2f}")
-
-            pose_list = [self.transform_pose(insertion_pose)]
-            insertion_pose.pose.position.z = FIRST_DEPTH
-            pose_list.append(self.transform_pose(insertion_pose))
-            self.move_group.set_start_state_to_current_state()
-
-            self.cartesian_plan_and_execute(pose_list,
-                                            msg=f" for z={insertion_pose.pose.position.z:.2f}")
-
-            pose = self.move_group.get_current_pose().pose
-            pose_list = [deepcopy(pose)]
-            x0 = pose.position.x
-            pose.position.x += 0.005
-            pose_list.append(pose)
-            self.move_group.set_start_state_to_current_state()
-            self.cartesian_plan_and_execute(pose_list,
-                                            msg=f" for Δx={pose.position.x - x0:.2f}")
-
-
-
-        except MoveitFailure:
-            rospy.logerr("Planning pipeline failed.")
+    def plan_and_execute(self, target=None, msg=''):
+        success, plan, planning_time, error_code = self.move_group.plan(target)
+        rospy.loginfo(
+            f"{'Successfully' if success else 'Failed'} plan {msg}" + (
+                f"in {planning_time:.4f}s, i.e {1.0 / planning_time:.2f} Hz" if success else f"with {self.error_code_to_string(error_code)}"
+            ))
+        if success:
+            t_exec = self.execute(plan)
+            if t_exec is not None:
+                rospy.loginfo(f'Executed trajectory in {t_exec}s.')
+            else:
+                rospy.loginfo(f'Trajectory execution failed.')
+                raise MoveitFailure()
+        else:
+            raise MoveitFailure()
 
     def cartesian_plan_and_execute(self, pose_list, msg):
         t_start = time.time()
@@ -143,43 +142,65 @@ class Orchestrator:
             rospy.loginfo(f'Trajectory execution failed.')
             raise MoveitFailure()
 
-    def plan_and_execute(self, target=None, msg=''):
-        success, plan, planning_time, error_code = self.move_group.plan(target)
-        rospy.loginfo(
-            f"{'Successfully' if success else 'Failed'} plan {msg}" + (
-                f"in {planning_time:.4f}s, i.e {1.0 / planning_time:.2f} Hz" if success else f"with {self.error_code_to_string(error_code)}"
-            ))
-        if success:
-            t_exec = self.execute(plan)
-            if t_exec is not None:
-                rospy.loginfo(f'Executed trajectory in {t_exec}s.')
-            else:
-                rospy.loginfo(f'Trajectory execution failed.')
-                raise MoveitFailure()
-        else:
-            raise MoveitFailure()
 
-    def execute(self, plan) -> Optional[float]:
-        t_start = time.time()
-        success = self.move_group.execute(plan, wait=True)
-        return time.time() - t_start if success else None
+def insertion_routine(orc):
+    orc.set_robot_state(orc.zero_joint_state)
 
-    # noinspection DuplicatedCode
-    @staticmethod
-    def error_code_to_string(code: Union[moveit_msgs.msg.MoveItErrorCodes, int]):
-        if isinstance(code, moveit_msgs.msg.MoveItErrorCodes):
-            code = code.val
-        codes = moveit_msgs.msg.MoveItErrorCodes
-        for name, val in codes.__dict__.items():
-            if val == code:
-                return name
-        rospy.logwarn(f"Invalid moveit error code: {code}")
-        return 'InvalidMOVEITError'
+    insertion_pose = geometry_msgs.msg.PoseStamped()
+    insertion_pose.header.frame_id = "Insertion_Pose"
+    insertion_pose.pose.orientation.w = 1
+
+    try:
+        insertion_pose.pose.position.z = -INITIAL_HEIGHT
+        orc.move_group.set_start_state(RobotState(joint_state=orc.zero_joint_state))
+        orc.plan_and_execute(target=orc.transform_pose(insertion_pose),
+                             msg=f" for z={insertion_pose.pose.position.z:.2f}")
+
+        insertion_pose.pose.position.z = -SECOND_HEIGHT
+        orc.move_group.set_start_state_to_current_state()
+        orc.plan_and_execute(target=orc.transform_pose(insertion_pose),
+                             msg=f" for z={insertion_pose.pose.position.z:.2f}")
+
+        pose_list = [orc.transform_pose(insertion_pose)]
+        insertion_pose.pose.position.z = FIRST_DEPTH
+        pose_list.append(orc.transform_pose(insertion_pose))
+        orc.move_group.set_start_state_to_current_state()
+
+        orc.cartesian_plan_and_execute(pose_list,
+                                       msg=f" for z={insertion_pose.pose.position.z:.2f}")
+
+        pose = orc.move_group.get_current_pose().pose
+        pose_list = [deepcopy(pose)]
+        x0 = pose.position.x
+        pose.position.x += 0.005
+        pose_list.append(pose)
+        orc.move_group.set_start_state_to_current_state()
+        orc.cartesian_plan_and_execute(pose_list,
+                                       msg=f" for Δx={pose.position.x - x0:.2f}")
+
+    except MoveitFailure:
+        rospy.logerr("Planning pipeline failed.")
+
+
+def interior_motion_routine(orc):
+    orc.set_robot_state(orc.inserted_joint_state)
+
+    try:
+        pose = orc.move_group.get_current_pose().pose
+        pose_list = [deepcopy(pose)]
+        x0 = pose.position.x
+        pose.position.x += 0.005
+        pose_list.append(pose)
+        orc.move_group.set_start_state_to_current_state()
+        orc.cartesian_plan_and_execute(pose_list,
+                                       msg=f" for Δx={pose.position.x - x0:.2f}")
+    except MoveitFailure:
+        rospy.logerr("Planning pipeline failed.")
 
 
 def main():
     orc = Orchestrator()
-    orc.run()
+    interior_motion_routine(orc)
 
 
 if __name__ == '__main__':
