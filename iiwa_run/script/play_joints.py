1be80e9af6d6da9ef7cf0c4a4d43a76c9b6ac968
Suraj Rathi <suraj.rathi00@gmail.com>
2023-04-14 13:31:01 +0530

    Add rcm visualization to play_joints

--- iiwa_run/script/play_joints.py
+++ iiwa_run/script/play_joints.py
@@ -1,8 +1,33 @@
 #! /usr/bin/python3
 import numpy as np
 import rospy
+from geometry_msgs.msg import Point, Quaternion, Vector3
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Header
+from std_msgs.msg import Header, ColorRGBA
+from visualization_msgs.msg import MarkerArray, Marker
+
+from specifications import from_param
+
+
+def show_rcm(pub):
+    spec = from_param()
+
+    # RCM
+    m = Marker()
+    m.header.frame_id = "Insertion_Pose"
+    m.header.stamp = rospy.Time.now()
+    m.ns = "viz"
+    m.id = 0
+    m.type = Marker.CYLINDER
+    m.action = Marker.ADD
+    m.pose.position = Point(x=0, y=0, z=0)
+    m.pose.orientation = Quaternion(w=1)
+    m.scale = Vector3(x=2 * spec.R, y=2 * spec.R, z=0.001)
+    m.color = ColorRGBA(r=1, g=0, b=0, a=0.5)
+
+    marker_array = MarkerArray()
+    marker_array.markers = [m]
+    pub.publish(marker_array)
 
 
 def main():
@@ -12,6 +37,8 @@ def main():
 
     rospy.init_node("path_player")
     joint_pub = rospy.Publisher('/move_group/fake_controller_joint_states', JointState, queue_size=5)
+    viz_pub = rospy.Publisher('/viz/volumes', MarkerArray, queue_size=1, latch=True)
+    show_rcm(viz_pub)
     active_joints = [
         'iiwa_joint_1',
         'iiwa_joint_2',
