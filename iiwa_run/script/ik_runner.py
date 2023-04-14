4280dc476919af48e983852365f96da00b170919
Suraj Rathi <suraj.rathi00@gmail.com>
2023-04-14 10:38:44 +0530

    remove numpy functions to improve speed

--- iiwa_run/script/ik_runner.py
+++ iiwa_run/script/ik_runner.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import random
 import sys
+from math import sqrt, acos, sin, cos, floor
 from pathlib import Path
 from queue import Queue
-from typing import Tuple, Optional
+from typing import Tuple
 
 import kdl_parser_py.urdf as kdl_parser
 import numpy as np
@@ -10,7 +12,6 @@ import rospkg
 import tqdm
 from PyKDL import ChainFkSolverPos_recursive, JntArray, Frame, ChainIkSolverPos_NR, ChainIkSolverVel_pinv, Vector, \
     Rotation
-from numpy.random import Generator
 from tf.transformations import quaternion_about_axis, quaternion_multiply
 
 from no_print import NoPrint
@@ -35,7 +36,7 @@ class Indexer:
     """ Maps the real world xyz coordinates to the indexes for the backing array. """
 
     def __init__(self, spec: Specifications, res: float):
-        s = spec.rl / np.sqrt(2)
+        s = spec.rl / sqrt(2)
         self.res = res
 
         self.x0, self.y0, self.z0 = spec.rcm
@@ -50,9 +51,9 @@ class Indexer:
         self.zlim = Lim(self.z0, self.z0 + spec.H)
 
         self.shape = (
-            int(np.floor(self.xlim.delta / res)),
-            int(np.floor(self.ylim.delta / res)),
-            int(np.floor(self.zlim.delta / res)),
+            int(floor(self.xlim.delta / res)),
+            int(floor(self.ylim.delta / res)),
+            int(floor(self.zlim.delta / res)),
         )
 
         self.xilim = Lim(0, self.shape[0] - 1)
@@ -74,9 +75,9 @@ class Indexer:
     def coord_to_index(self, x, y, z):
         if self.xlim(x) and self.ylim(y) and self.zlim(z):
             return (
-                int(np.floor(((x - self.x0) + self.xoff) / self.res)),
-                int(np.floor(((y - self.y0) + self.yoff) / self.res)),
-                int(np.floor(((z - self.z0) + self.zoff) / self.res)),
+                int(floor(((x - self.x0) + self.xoff) / self.res)),
+                int(floor(((y - self.y0) + self.yoff) / self.res)),
+                int(floor(((z - self.z0) + self.zoff) / self.res)),
             )
 
         raise IndexError(f"{x}\t{y}\t{z}")
@@ -95,7 +96,8 @@ class IKOrchestrator:
         self.res = resolution
         self.indexer = Indexer(self.spec, self.res)
 
-        self.rng: Generator = np.random.default_rng(seed=self.spec.seed)
+        random.seed(self.spec.seed)
+        self.rng_state = random.getstate()
         self.num_inner = self.spec.n_inner
 
         # Set up KDL
@@ -168,6 +170,9 @@ class IKOrchestrator:
                         key=lambda k: k[0]
                     )
                 # save data
+                if (joints.rows()) != self.nj:
+                    print(joints, (joints.rows()))
+                    raise RuntimeError()
                 self.arr[ind] = [joint_diff, pos_error, orien_error] + [joints[i] for i in range(self.nj)]
 
                 # print(f"Initial:\t{orc.indexer.x0:.2f}\t{orc.indexer.y0:.2f}\t{orc.indexer.z0:.2f}")
@@ -176,10 +181,10 @@ class IKOrchestrator:
 
                 self.add_next(ind)
                 bar.update(1)
-                bar.set_description(f"F: {self.frontier.qsize()}")
+                # bar.set_description(f"F: {self.frontier.qsize()}")
 
     def get_target_orientation(self, target_point: Vector,
-                               start_point: Optional[np.ndarray] = np.array([0, 0, 0])) -> Rotation:
+                               start_point: Tuple[float, float, float] = (0, 0, 0)) -> Rotation:
         dx = target_point.x() - self.insertion_pt.x() - start_point[0]
         dy = target_point.y() - self.insertion_pt.y() - start_point[1]
         dz = target_point.z() - self.insertion_pt.z() - start_point[2]
@@ -194,18 +199,14 @@ class IKOrchestrator:
                     f"Invalid interior point: {target_point} compared to insertion point: {self.insertion_pt}")
                 raise RuntimeError()
 
-        orig = np.array((0, 0, -1))
-        new = np.array((dx, dy, dz))
-        new /= np.linalg.norm(new)
+        nn = sqrt(dx ** 2 + dy ** 2 + dz ** 2)
+        nx, ny, nz = (dx / nn, dy / nn, dz / nn)
 
-        n_c = np.cross(orig, new)
-        n_c /= np.linalg.norm(n_c)
+        nn = sqrt(ny ** 2 + nx ** 2)
 
-        theta = np.arccos(np.dot(orig, new))
-        q_rot = quaternion_about_axis(axis=n_c, angle=theta)
+        q_rot = quaternion_about_axis(angle=acos(-nz), axis=(-ny / nn, -nx / nn, 0))
 
-        q_x, q_y, q_z, q_w = self.insertion_rot.GetQuaternion()
-        q_initial = np.array((q_x, q_y, q_z, q_w))
+        q_initial = self.insertion_rot.GetQuaternion()
         q_net = quaternion_multiply(q_rot, q_initial)
 
         return Rotation().Quaternion(x=q_net[0], y=q_net[1], z=q_net[2], w=q_net[3])
@@ -251,7 +252,7 @@ class IKOrchestrator:
             dot_prod += d * d_c
         if dot_prod > 1:
             dot_prod = 1.0
-        orientation_error = np.arccos(dot_prod)
+        orientation_error = acos(dot_prod)
 
         return joint_diff, position_error, orientation_error, j_ik
 
@@ -268,44 +269,52 @@ class IKOrchestrator:
                 index[ax] -= val
 
     def generate_frames(self, ind):
-        rng: Generator = np.random.default_rng(self.rng.integers(0, 2 ** 31 - 1, endpoint=True, dtype=np.int32))
+        random.setstate(self.rng_state)
+        this_seed = random.getrandbits(32)
+        self.rng_state = random.getstate()
+        random.seed(this_seed)
+
         center_r = self.spec.R - self.spec.r
 
         target_coords = self.indexer.index_to_coord(*ind)
+        px, py, pz = target_coords
 
         def get_locs():
-            yield np.array((0, 0, 0))
+            yield 0, 0, 0
             while True:
-                r, theta = rng.random(2)
+                r, theta = random.random(), random.random()
                 r *= center_r
                 theta *= 2 * np.pi
 
-                yield np.array((r * np.cos(theta), r * np.sin(theta), 0))
+                yield r * cos(theta), r * sin(theta), 0
 
-        N = np.linalg.norm
-
-        def dist_impl(O, Q, P):
-            return (self.spec.R - N(Q - O)) * N(np.cross(P - Q, O - Q)) / N(P - Q) / N(O - Q)
-
-        def is_valid(loc: np.ndarray) -> bool:
-            O = np.array((self.insertion_pt.x(), self.insertion_pt.y(), self.insertion_pt.z()))
-            Q = O + loc
-            P = np.array(target_coords)
-
-            if N(Q - O) <= 1e-9:
+        def is_valid(lx: float, ly: float, lz: float) -> bool:
+            n_qo_s = (lx ** 2 + ly ** 2 + lz ** 2)
+            n_qo = sqrt(n_qo_s)
+            if n_qo <= 1e-9:
                 return True
 
-            return dist_impl(O, Q, P) > self.spec.r
+            ox, oy, oz = self.insertion_pt.x(), self.insertion_pt.y(), self.insertion_pt.z()
+            qx, qy, qz = ox + lx, oy + ly, oz + lz
+
+            n_pq_s = sqrt((qx - px) ** 2 + (qy - py) ** 2 + (qz - pz) ** 2)
+
+            ux, uy, uz = px - qx, py - qy, pz - qz
+
+            cross_s = (uy * lz - uz * ly) ** 2 + (uz * lx - ux * lz) ** 2 + (ux * ly - uy * lx) ** 2
+
+            d = (self.spec.R - n_qo) * sqrt(cross_s / n_pq_s / n_qo_s)
+            return d > self.spec.r
 
         # Method 1: Brute force
 
         i = 0
-        for loc in get_locs():
-            if is_valid(loc):
+        for lx, ly, lz in get_locs():
+            if is_valid(lx, ly, lz):
                 i += 1
                 f = Frame()
                 f.p = Vector(*target_coords)
-                f.M = self.get_target_orientation(f.p, start_point=loc)
+                f.M = self.get_target_orientation(f.p, start_point=(lx, ly, lz))
                 yield f
 
             if i >= self.num_inner:
