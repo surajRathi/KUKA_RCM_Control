ab411777bd1dbed45475402e537850d80237add5
Suraj Rathi <suraj.rathi00@gmail.com>
2023-04-23 14:54:43 +0530

    refactor and add circular paths

--- iiwa_run/iiwa_run/ik_path.py
+++ iiwa_run/iiwa_run/ik_path.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 import time
 from math import isfinite
+from typing import Tuple
 
 import numpy as np
 from PyKDL import JntArray
@@ -33,15 +34,16 @@ class PathToJoints(SamplingIKOrchestrator):
         start_angle = np.arctan2(start[2] - center[2], start[1] - center[1])
         angles = np.arange(start_angle, start_angle + angle, d_theta)
 
-        return center + (r * np.cos(angles), r * np.sin(angles))
+        ret = np.zeros((angles.shape[0], 3))
+        ret[:, 0] = r * np.cos(angles)
+        ret[:, 1] = r * np.sin(angles)
+
+        return center + ret
 
     def run(self, path):
-        t_start = time.time()
         assert all(path[0, :] == (0, 0, 0))
-        tjd_s = np.zeros(path.shape[0]) * np.nan
         joints = np.zeros((path.shape[0], self.nj)) * np.nan
 
-        tjd_s[0] = 0
         joints[0, :] = self.initial_joint_states
 
         path = self.r0 + path
@@ -51,28 +53,46 @@ class PathToJoints(SamplingIKOrchestrator):
         for i, pt in enumerate(tqdm(path[1:], total=path.shape[0] - 1), start=1):
             joint_diff, pos_error, orien_error, next_joints = self.get_solution(cur_joints, pt)
             if not isfinite(joint_diff):
-                print(f"Could not find solution at {i=} of {path.shape[0]}")
-                print(f"Loc: {path[i, :]}")
                 break
-            tjd_s[i] = np.degrees(np.sqrt(joint_diff))
             joints[i, :] = [next_joints[i] for i in range(self.nj)]
 
             cur_joints = next_joints
-        else:
-            t_end = time.time()
-            print(f"Path Successfully found in {(t_end - t_start) * 1000:.0f}ms.")
-            print(f"n_inner: {self.num_inner}")
-            print(f"Mean TJDs: {tjd_s.mean():.4f}")
 
         return joints
 
 
 def main():
     pc = PathToJoints()
-    # path = pc.get_line_path((0.0, 0, 0), (0.3, 0.0, 0.0))
-    path = np.vstack((pc.get_line_path((0, 0, 0), (0.3, 0.0, 0.0)), pc.get_line_path((0.3, 0, 0), (0.0, 0.3, 0.0))))
+    # path = pc.get_line_path((0.0, 0, 0), (0.25, 0.0, 0.0))
+    path = np.vstack((
+        pc.get_line_path((0, 0, 0), (0.25, 0.0, 0.0)),
+        pc.get_circle_path((0, 0, 0), (0.25, 0.0, 0.0)),
+        pc.get_line_path((0.25, 0, 0), (0.0, 0.0, 0.0))))
+
+    t_start = time.time()
     joints = pc.run(path)
-    np.save('joint_vals.npy', joints)
+    t_end = time.time()
+
+    def index(array, item) -> Tuple[int, ...]:
+        for idx, val in np.ndenumerate(array):
+            if val == item:
+                return idx
+        return tuple([-1] * len(array.shape))
+
+    if (i := index(joints, np.nan)) != -1:
+        print(f"Path Successfully found in {(t_end - t_start) * 1000:.0f}ms.")
+        print(f"n_inner: {pc.num_inner}")
+
+        initial_joints = np.array(pc.initial_joint_states).reshape(1, -1)
+        tjd_s = np.degrees(np.sqrt(
+            (np.diff(joints, axis=0, prepend=initial_joints) ** 2).sum(axis=-1)
+        ))
+        print(f"Mean TJDs: {tjd_s.mean():.4f}")
+
+        np.save('joint_vals.npy', joints)
+    else:
+        print(f"Could not find solution at {i=} of {path.shape[0]}")
+        print(f"Loc: {path[i, :]}")
 
 
 if __name__ == '__main__':
